dac580a4fb59748490b597811205d13c6a52bb6f
Iago Fragnan <n3rdydzn@hotmail.com>
2026-07-26 15:40:15 -0300

    feat: placeholder for the new portfolio

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import "./globals.css";
+import { Nunito_Sans } from 'next/font/google'
 
+const nunito = Nunito_Sans({ subsets: ['latin'] })
 
 export const metadata = {
   title: "Iago Fragnan - Portfolio",
@@ -8,10 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="pt-BR"
-      className={`h-full antialiased`}
-    >
+    <html lang="pt-BR" className={`${nunito.className} h-full antialiased`} >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
